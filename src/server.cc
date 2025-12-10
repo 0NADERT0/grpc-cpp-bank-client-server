af4696334d1df79648578cb4861b7df8988c6b99
NADERT <repin.ve@gmail.com>
2025-12-10 07:31:01 +0000

    rework pqxx

--- src/server.cc
+++ src/server.cc
@@ -21,7 +21,7 @@ void load_env(const std::string& filename = ".env") {
         if (equal_pos != std::string::npos) {
             std::string key = line.substr(0, equal_pos);
             std::string value = line.substr(equal_pos + 1);
-            setenv(key.c_str(), value.c_str(), true); // Overwrite if exists
+            setenv(key.c_str(), value.c_str(), true);
         }
     }
 }
@@ -38,7 +38,7 @@ pqxx::connection conn("user=" + db_user +
                         " host=" + db_host +
                         " port=" + db_port);
 
-pqxx::work txn(conn);
+//pqxx::work txn(conn); // Delete this
 
 class PaymentServiceImpl final : public payment::PaymentService::Service {
     grpc::Status TransferMoney(grpc::ServerContext* context, const payment::TransferRequest* request, payment::TransferResponse* response) override {
@@ -47,6 +47,7 @@ class PaymentServiceImpl final : public payment::PaymentService::Service {
         double amount = request->amount();
 
         try {
+            pqxx::work txn(conn);
             pqxx::result sender_balance_result = txn.exec_params(
                 "SELECT balance FROM users WHERE user_id = $1", sender_id);
             if (sender_balance_result.size() == 0) {
@@ -82,6 +83,7 @@ class PaymentServiceImpl final : public payment::PaymentService::Service {
     grpc::Status CheckBalance(grpc::ServerContext* context, const payment::BalanceRequest* request, payment::BalanceResponse* response) override {
         int sender_id = request->user_id();
         try {
+            pqxx::work txn(conn);
             pqxx::result sender_balance_result = txn.exec_params(
                 "SELECT balance FROM users WHERE user_id = $1", sender_id);
             double sender_balance = sender_balance_result[0][0].as<double>();
@@ -97,6 +99,7 @@ class PaymentServiceImpl final : public payment::PaymentService::Service {
     grpc::Status GetTransactionHistory(grpc::ServerContext* context, const payment::HistoryRequest* request, payment::HistoryResponse* response) override {
         int sender_id = request->user_id();
         try {
+            pqxx::read_transaction txn(conn);
             pqxx::result sender_transactions = txn.exec_params(
                 "SELECT * FROM transactions WHERE sender_id = $1", sender_id);
             pqxx::result receiver_transactions = txn.exec_params(
@@ -104,9 +107,9 @@ class PaymentServiceImpl final : public payment::PaymentService::Service {
 
             for (const auto& row : sender_transactions) {
                 payment::Transaction* transaction = response->add_transactions();
-                transaction->set_transaction_id(row["transaction_id"].as<int32_t>());
-                transaction->set_sender_id(row["sender_id"].as<int32_t>());
-                transaction->set_receiver_id(row["receiver_id"].as<int32_t>());
+                transaction->set_transaction_id(row["transaction_id"].as<int>());
+                transaction->set_sender_id(row["sender_id"].as<int>());
+                transaction->set_receiver_id(row["receiver_id"].as<int>());
                 transaction->set_amount(row["amount"].as<double>());
                 transaction->set_timestamp(row["timestamp"].as<std::string>());
                 transaction->set_status(row["status"].as<std::string>());
@@ -114,9 +117,9 @@ class PaymentServiceImpl final : public payment::PaymentService::Service {
 
             for (const auto& row : receiver_transactions) {
                 payment::Transaction* transaction = response->add_transactions();
-                transaction->set_transaction_id(row["transaction_id"].as<int32_t>());
-                transaction->set_sender_id(row["sender_id"].as<int32_t>());
-                transaction->set_receiver_id(row["receiver_id"].as<int32_t>());
+                transaction->set_transaction_id(row["transaction_id"].as<int>());
+                transaction->set_sender_id(row["sender_id"].as<int>());
+                transaction->set_receiver_id(row["receiver_id"].as<int>());
                 transaction->set_amount(row["amount"].as<double>());
                 transaction->set_timestamp(row["timestamp"].as<std::string>());
                 transaction->set_status(row["status"].as<std::string>());
@@ -133,6 +136,7 @@ class PaymentServiceImpl final : public payment::PaymentService::Service {
         int sender_id = request->user_id();
         double amount = request->amount();
         try {
+            pqxx::work txn(conn);
             txn.exec_params("UPDATE users SET balance = balance + $1 WHERE user_id = $2", amount, sender_id);
 
             txn.exec_params("INSERT INTO transactions (sender_id, receiver_id, amount, status) VALUES ($1, $2, $3, 'deposit')",
@@ -150,6 +154,7 @@ class PaymentServiceImpl final : public payment::PaymentService::Service {
         int sender_id = request->user_id();
         double amount = request->amount();
         try {
+            pqxx::work txn(conn);
             pqxx::result sender_balance_result = txn.exec_params(
                 "SELECT balance FROM users WHERE user_id = $1", sender_id);
             double sender_balance = sender_balance_result[0][0].as<double>();
